7dd7d0aa2cb7af98f0c41f760417cc44b769fa8f
Eivind Lie Andreassen <ela4991@gmail.com>
2019-06-03 18:18:42 +0200

    Change bookmark button and add it to bottom comicNav

--- bookmark.js
+++ bookmark.js
@@ -2,6 +2,7 @@ const storage = browser.storage.sync;
 const COUNT_LIMIT = 5;
 
 var comicNav = document.getElementsByClassName("comicNav")[0];
+var comicNav2 = document.getElementsByClassName("comicNav")[1];
 var previousLink = comicNav.children[1].children[0];
 var linkList = previousLink.href.split("/");
 var previousComic = parseInt(linkList[linkList.length - 2]);
@@ -57,12 +58,11 @@ storage.get("read_strips").then((result) => {
 
     var bookmarkListItem = document.createElement("li");
     var bookmarkLink = document.createElement("a");
-    var linkText = document.createTextNode("Go to bookmark");
+    var linkText = document.createTextNode("< Bookmark");
     bookmarkLink.appendChild(linkText);
     bookmarkLink.href = "/" + bookmarkIndex;
     bookmarkListItem.appendChild(bookmarkLink);
 
-    comicNav.insertBefore(bookmarkListItem, comicNav.children[3]);
-
-
+    comicNav.insertBefore(bookmarkListItem, comicNav.children[2]);
+    comicNav2.insertBefore(bookmarkListItem, comicNav2.children[2]);
 });
